6d9e8ef0ceb9cca7990a3dbb80106e485f24e8fa
Brian Han <brianhan@brians-mbp.lan>
2020-12-04 22:42:28 -0800

    fixed hover state for SHOP NOW arrow

--- src/component/SliderText.js
+++ src/component/SliderText.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import arrow from '../img/icon-arrow.svg'
+//import arrow from '../img/icon-arrow.svg'
 
 export default function SliderText() {
   return (
@@ -10,7 +10,11 @@ export default function SliderText() {
           Our experts combine form and function in bringing your vision to life. Create a room in your 
           own style with our collection and make your property a reflection of you and what you love.</p>
         <div className="shopLinkContainer">
-          <a className="shopLink" href="shop">SHOP NOW <span className="arrowContainer"><img className="arrowLink" src={arrow} alt="arrow"/></span></a>
+          <a className="shopLink" href="shop">SHOP NOW <span className="arrowContainer">
+            <svg className="arrowLink" fill="#000" width="40" height="12" xmlns="http://www.w3.org/2000/svg">
+              <path className="arrowPath" d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fillRule="nonzero"/>
+            </svg>
+          </span></a>
         </div>
       </div>
     </div>
