a39011c50082913b37fd7b46db0505b36b5c1797
Brian Han <brianhan@brians-mbp.lan>
2020-12-05 18:17:26 -0800

    fixed JSX typo

--- src/component/SliderText.js
+++ src/component/SliderText.js
@@ -12,7 +12,7 @@ export default function SliderText() {
             Our experts combine form and function in bringing your vision to life. Create a room in your 
             own style with our collection and make your property a reflection of you and what you love.</p>
           <div className="shopLinkContainer">
-            <a tabindex="-1" className="shopLink" href="shop">SHOP NOW <span className="arrowContainer">
+            <a tabIndex="-1" className="shopLink" href="shop">SHOP NOW <span className="arrowContainer">
               <svg className="arrowLink" fill="#000" width="40" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path className="arrowPath" d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fillRule="nonzero"/>
               </svg>
@@ -26,7 +26,7 @@ export default function SliderText() {
             Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
             store locator. Any questions? Don't hesitate to contact us today.</p>
           <div className="shopLinkContainer">
-            <a tabindex="-1" className="shopLink" href="shop">SHOP NOW <span className="arrowContainer">
+            <a tabIndex="-1" className="shopLink" href="shop">SHOP NOW <span className="arrowContainer">
               <svg className="arrowLink" fill="#000" width="40" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path className="arrowPath" d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fillRule="nonzero"/>
               </svg>
@@ -40,7 +40,7 @@ export default function SliderText() {
             to ensure that every product is made as perfect and as consistent as possible. With three decades of 
             experience in this industry, we understand what customers want for their home and office.</p>
           <div className="shopLinkContainer">
-            <a tabindex="-1" className="shopLink" href="shop">SHOP NOW <span className="arrowContainer">
+            <a tabIndex="-1" className="shopLink" href="shop">SHOP NOW <span className="arrowContainer">
               <svg className="arrowLink" fill="#000" width="40" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path className="arrowPath" d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fillRule="nonzero"/>
               </svg>
